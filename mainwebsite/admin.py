5bf61b772a0bfe38358c20f135eeba58bbb93ba4
Tanmoy Sarkar <ts741127@gmail.com>
2021-08-28 21:10:03 +0530

    Update

--- mainwebsite/admin.py
+++ mainwebsite/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
 from mainwebsite.models import *
+from django.utils.html import format_html
 
-admin.site.register(Artists)
+
+class ArtistsAdmin(admin.ModelAdmin):
+    def artworks(self):
+        html = ""
+        for i in self.artwork_of_artist.all():
+            html += f"<a href=\"/admin/mainwebsite/artwork/{i.id}/change/\" target=\"__blank\">Picture Id {i.id}</a><br>"
+        return  format_html(html)
+    artworks.allow_tags = True
+    list_display = ["name",artworks]
+    search_fields = ["name"]
+
+
+admin.site.register(Artists, ArtistsAdmin)
+
 admin.site.register(Artwork)
 admin.site.register(ArtworkSize)
 admin.site.register(OrderHistory)
